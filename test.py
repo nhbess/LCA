4ef2798fe2da42066ef759b1b574f52a7ec4269f
NB <nhbessone@gmail.com>
2024-09-27 21:41:08 +0200

    minor

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import torch
-n_production_rules = 1
+torch.set_printoptions(precision=1, sci_mode=False)
 
-production_rules = torch.rand(n_production_rules * 2 * 2 * 2, requires_grad=True) * 2 - 1
+n_production_rules = 2
+
+production_rules = torch.rand(n_production_rules * 2 * 3 * 3, requires_grad=True) * 2 - 1
 print(f'production_rules:\n{production_rules}')
 
 reactants, products = torch.split(production_rules, production_rules.shape[0] // 2)
@@ -45,16 +47,30 @@ print(f'mapped_reactants:\n{mapped_reactants}')
 
 # BOARD
 print('BOARD ----------------')
-board = torch.zeros((4, 4), dtype=torch.float32, requires_grad=True) + 1e-12
+board = torch.rand((4, 4), dtype=torch.float32, requires_grad=True)*2-1
+board = 1/(1+torch.exp(-board*M))
 print(f'board:\n{board}')
 
 new_board = torch.zeros_like(board, requires_grad=True)
 # i need to read all the subgrids of the board and compare them with the reactants
 
-for i in range(board.shape[0] - 1):
-    for j in range(board.shape[1] - 1):
-        subgrid = board[i:i+2, j:j+2]
-        print(f'subgrid:\n{subgrid}')
-        if torch.allclose(subgrid, reactants.view(2, 2)):
-            new_board[i:i+2, j:j+2] = mapped_products.view(2, 2)
-            print(f'new_board:\n{new_board}')
+mapped_reactants = mapped_reactants.view(1, 2, 3, 3)
+mapped_products = mapped_products.view(1, 2, 3, 3)
+print(f'mapped_reactants:\n{mapped_reactants}')
+print(f'mapped_products:\n{mapped_products}')
+for i in range(board.shape[0] - 2):
+    for j in range(board.shape[1] - 2):
+        subgrid = board[i:i+3, j:j+3]
+        #print(f'subgrid:\n{subgrid}')
+        for k in range(len(mapped_reactants[0])):
+            continue
+            reactant = mapped_reactants[0][k]
+            product = mapped_products[0][k]
+
+            #print(f'reactant:\n{reactant}')
+            error_matrix = torch.abs(subgrid - reactant)
+            #print(f'error_matrix:\n{error_matrix}') 
+            total_error = torch.sum(error_matrix)
+            total_error = torch.exp(-total_error*total_error*M/2)
+            new_board[i:i+3, j:j+3] += total_error
+            print(f'total_error:\n{total_error}')
